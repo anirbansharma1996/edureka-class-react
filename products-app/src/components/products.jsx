2d3af342a089c32d2bd5fe59b0d8a28068f25f0e
Anirban <anirbansharma36@gmail.com>
2023-10-06 18:59:36 +0530

    updated

--- products-app/src/components/products.jsx
+++ products-app/src/components/products.jsx
@@ -1,22 +1,30 @@
 import { useState } from "react";
 
 export default function Products() {
-  const [text,setText] = useState("")
-  const [array,setArray] = useState([])
-   
-  const handleInput=(e)=>{
-    setText(e.target.value)
-  }
-  const handleAdd=()=>{
-    setArray([...array,text])
-  }
-  console.log(array)
+  const [text, setText] = useState("");
+  const [array, setArray] = useState([]);
+
+  const handleInput = (e) => {
+    setText(e.target.value);
+  };
+  const handleAdd = () => {
+    if (text === "") {
+      alert("enter something first");
+    } else {
+      setArray([...array, text]);
+      setText("");
+    }
+  };
 
   return (
-  <div>
-    <input type="text" onChange={handleInput} />
-    <button onClick={handleAdd}>ADD</button>
-
-  </div>
+    <div>
+      <input type="text" value={text} onChange={handleInput} />
+      <button onClick={handleAdd}>ADD</button>
+      <ol>
+        {array.map((el) => (
+          <li>{el}</li>
+        ))}
+      </ol>
+    </div>
   );
 }
