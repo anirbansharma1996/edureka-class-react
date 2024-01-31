30d619791bf9f94e36b35e02fda4ace70a23daa1
Anirban <anirbansharma36@gmail.com>
2024-01-31 19:57:18 +0530

    added cors

--- Authentication/server/server.js
+++ Authentication/server/server.js
@@ -1,19 +1,16 @@
-const express = require('express')
-const connectDB = require("./utlis/db.js")
-const AuthRoute = require('./route/auth.route.js')
+const express = require("express");
+const connectDB = require("./utlis/db.js");
+const cors = require("cors");
+const AuthRoute = require("./route/auth.route.js");
 
-const bcrypt = require("bcrypt")
+const PORT = 8008;
+const app = express();
+app.use(cors());
+app.use(express.json());
 
+app.use("/v1", AuthRoute);
 
-const PORT = 8008
-const app = express()
-app.use(express.json())
-
-app.use("/v1",AuthRoute)
-
-app.listen(PORT,()=>{
-    connectDB()
-    console.log(`http://127.0.0.1:${PORT}`)
-})
-
-
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`http://127.0.0.1:${PORT}`);
+});
